0d511ada93056c4dc6e3d7e71793e8c8b3277b9f
Martin Petit <martin.petit103@gmail.com>
2021-12-15 14:38:43 +0000

    add create, update, delete components and links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './general/header/header.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserAddComponent } from './components/user-add/user-add.component';
+import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { UserDeleteComponent } from './components/user-delete/user-delete.component';
 
 const routes: Routes = [
     { path: 'header', component: HeaderComponent},
     { path: 'users', component: UsersListComponent},
+    { path: 'user-add', component: UserAddComponent},
+    { path: 'user-update', component: UserUpdateComponent},
+    { path: 'user-delete', component: UserDeleteComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './general/header/header.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserAddComponent } from './components/user-add/user-add.component';
+import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { UserDeleteComponent } from './components/user-delete/user-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UsersListComponent
+    UsersListComponent,
+    UserAddComponent,
+    UserUpdateComponent,
+    UserDeleteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
